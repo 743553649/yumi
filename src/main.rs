3f5101d078bfda504ca2ba69e2964c67b5e7826d
743553649 <128961187+743553649@users.noreply.github.com>
2026-08-05 10:10:40 +0800

    fix(ipc): remove extra args from ipc_server::start call

--- src/main.rs
+++ src/main.rs
@@ -83,12 +83,10 @@ fn main() -> Result<()> {
         let ipc_tx = tx.clone();
         let ipc_root = root.clone();
         let ipc_port = config.ipc.port;
-        let ipc_config_arc = Arc::clone(&config_arc);
-        let ipc_force_refresh_arc = Arc::clone(&force_refresh_arc);
         thread::Builder::new()
             .name("ipc_server".to_string())
             .spawn(move || {
-                ipc_server::start(ipc_tx, ipc_root, ipc_port, ipc_config_arc, ipc_force_refresh_arc);
+                ipc_server::start(ipc_tx, ipc_root, ipc_port);
             })?;
         info!("IPC server starting on port {}", config.ipc.port);
     }
